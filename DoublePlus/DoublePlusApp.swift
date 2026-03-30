a04e38d2d67a92718be671bfaef86ac15b9c86e8
nestcc <nestncc@outlook.com>
2026-03-30 14:46:39 +0800

    Initial Commit

---
+++ DoublePlus/DoublePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoublePlusApp.swift
+//  DoublePlus
+//
+//  Created by nestcc on 30/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct DoublePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
